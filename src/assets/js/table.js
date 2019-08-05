08fa3c6103ec01e023149ea8567a27aa4bfc8af6
klimovich008 <klimovich008@gmail.com>
2019-08-05 12:25:39 +0300

    renew code.

--- src/assets/js/table.js
+++ src/assets/js/table.js
@@ -1,40 +1,69 @@
-function check_group(contributions) {
-  if (contributions > 100)
+const Gold = 100;
+const Silver = 10;
+const Bronze = 0;
+
+function checkGroup(contributions) {
+  if (contributions > Gold)
     return "Gold";
   else
-    if (contributions > 10) return "Silver";
+    if (contributions > Silver) return "Silver";
   return "Bronze";
 }
-function do_modal(key, company, location, email) {
-  modal = "    <div class=\"modal fade\" id='user_" + key + "' tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"mySmallModalLabel\" aria-hidden=\"true\">\r\n      <div class=\"modal-dialog modal-sm\">\r\n        <div class=\"modal-content\">\r\n          <div class=\"col\">Company - " + company + " <\/div>\r\n          <div class=\"col\">Location - " + location + "<\/div>\r\n          <div class=\"col\">Email - " + email + "<\/div>\r\n        <\/div>\r\n      <\/div>\r\n    <\/div>"
+
+function createModal(key, val) {
+  $.getJSON("https://api.github.com/users/" + val.login, function (data) {
+    modal = generateModalForm(key, data.company, data.location, data.email)
+    $(modal).appendTo("#table");
+  });
+}
+function start() {
+  $.getJSON("https://api.github.com/repos/thomasdavis/backbonetutorials/contributors", function (data) {
+    let items = [];
+    let i = 0;
+    let modal = "";
+    $.each(data, function (key, val) {
+      items.push("<tr class = 'line' style='cursor: grab;' data-toggle='modal' data-target='#user_" + key + "' >" + 
+                    "<td id='" + key + "' >" + val.id + "</td> " + 
+                    "<td>" + val.login + "</td> " + 
+                    "<a><td>" + val.url + "</td> </a>" + 
+                    "<td>" + val.contributions + "</td> " + 
+                    "<td class = 'Group' value='" + checkGroup(val.contributions) + "'>" + checkGroup(val.contributions) + "</td> " 
+                 + "</tr>");
+      createModal(key, val);
+    });
+
+    $("<tbody/>", {
+      html: items.join("")
+    }).appendTo("#table");
+  })
+}
+
+
+
+function generateModalForm(key, company, location, email) {
+  modal = "<div class=\"modal fade\" id='user_" + key + "' tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"mySmallModalLabel\" aria-hidden=\"true\">\r\n \
+            <div class=\"modal-dialog modal-sm\">\r\n \
+              <div class=\"modal-content\">\r\n \
+                 <div class=\"col\">Company - " + company + "<\/div>\r\n \
+                 <div class=\"col\">Location - " + location + "<\/div>\r\n \
+                 <div class=\"col\">Email - " + email + "<\/div>\r\n \
+              <\/div>\r\n \
+            <\/div>\r\n \
+          <\/div>"
   return modal;
 }
-$.getJSON("https://api.github.com/repos/thomasdavis/backbonetutorials/contributors", function (data) {
-  var items = [];
-  var i = 0;
-  let modal = "";
-  $.each(data, function (key, val) {
-    $.getJSON("https://api.github.com/users/" + val.login, function (data) {
-      modal = do_modal(key, data.company, data.location, data.email)
-      $(modal).appendTo("#table");
-    });
-    items.push("<tr class = 'line' style='cursor: grab;' data-toggle='modal' data-target='#user_" + key + "' >" + "<td id='" + key + "' >" + val.id + "</td> " + "<td>" + val.login + "</td> " + "<a><td>" + val.url + "</td> </a>" + "<td>" + val.contributions + "</td> " + "<td class = 'Group' value='" + check_group(val.contributions) + "'>" + check_group(val.contributions) + "</td> " + "</tr>");
-  });         //document.body.append(elm);
-  $("<tbody/>", {
-    html: items.join("")
-  }).appendTo("#table");
-})
-function group_select(input) {
+
+function selectGroup(input) {
   let elements = document.querySelectorAll('.line');
   for (i = 0; i < elements.length; i++) {
     group = elements[i].querySelector(".Group");
-    group.innerHTML === input || input === "All" ? elements[i].style.display = "" : elements[i].style.display = "none";
+    elements[i].style.display = group.innerHTML === input || input === "All" ? "" : "none";
 
   }
 
 }
-function sort_select(dir) {
-  var i, switching, b, shouldSwitch, dir, switchcount = 0;
+function sortTable(dir) {
+  let i, switching, b, shouldSwitch, switchCount = 0;
   switching = true;
   while (switching) {
     switching = false;
@@ -56,15 +85,21 @@ function sort_select(dir) {
     if (shouldSwitch) {
       b[i].parentNode.insertBefore(b[i + 1], b[i]);
       switching = true;
-      switchcount++;
+      switchCount++;
     } else {
-      if (switchcount === 0 && dir === "ASC") {
+      if (switchCount === 0 && dir === "ASC") {
         dir = "DESC";
         switching = true;
       }
     }
   }
 }
+
+
+
+
 $(function () {
-  sort_select("ASC");
+  start();
+  sortTable("ASC");
+
 });
