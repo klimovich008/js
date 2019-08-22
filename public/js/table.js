5d834c466c30c41d2f960bd596d364825603cc6a
klimovich008 <klimovich008@gmail.com>
2019-08-22 16:38:42 +0300

    Knockout.js: revrited;

--- public/js/table.js
+++ public/js/table.js
@@ -1,19 +1,14 @@
 const Gold = 100;
 const Silver = 10;
 const Bronze = 0;
-function checkGroup(contributions) {
-  if (contributions > Gold)
-    return "Gold";
-  else
-    if (contributions > Silver) return "Silver";
-  return "Bronze";
-}
+let modals = ko.observableArray([]);
+let contributors = ko.observableArray([]);
 
 function createModal(key, val) {
+  let buf;
   $.getJSON("https://api.github.com/users/" + val.login)
     .done(function (data) {
-      modal = generateModalForm(key, data.company, data.location, data.email, data.avatar_url)
-      $(modal).appendTo("#contributorsTable");
+      modals.push(data);
     })
     .fail(function (jqxhr, textStatus, error) {
       var err = textStatus + ", " + error;
@@ -23,10 +18,14 @@ function createModal(key, val) {
 function start() {
   $.getJSON("https://api.github.com/repos/thomasdavis/backbonetutorials/contributors")
     .done(function (data) {
-      ko.applyBindings({ contributors : ko.observableArray(data)});
       $.each(data, function (key, val) {
         createModal(val.id, val);
+
       });
+      contributors = ko.observableArray(data);
+      ko.applyBindings({ contributors: contributors, modals: modals });
+      sortTable("ASC");
+
     })
     .fail(function (jqxhr, textStatus, error) {
       var err = textStatus + ", " + error;
@@ -34,23 +33,6 @@ function start() {
     });
 
 }
-
-
-
-function generateModalForm(key, company, location, email, image) {
-  modal = "<div class=\"modal fade\" id='" + key + "' tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"mySmallModalLabel\" aria-hidden=\"true\">\r\n \
-            <div class=\"modal-dialog modal-sm\">\r\n \
-              <div class=\"modal-content\">\r\n \
-                 <img src='" + image + "' width='100%'>    \
-                 <div class=\"col\">Company - " + company + "<\/div>\r\n \
-                 <div class=\"col\">Location - " + location + "<\/div>\r\n \
-                 <div class=\"col\">Email - " + email + "<\/div>\r\n \
-              <\/div>\r\n \
-            <\/div>\r\n \
-          <\/div>"
-  return modal;
-}
-
 function selectGroup(input) {
   let elements = document.querySelectorAll('#contributorsTable .line');
   for (i = 0; i < elements.length; i++) {
@@ -61,43 +43,10 @@ function selectGroup(input) {
 
 }
 function sortTable(dir) {
-  let i, switching, b, shouldSwitch, switchCount = 0;
-  switching = true;
-  while (switching) {
-    switching = false;
-    b = document.querySelectorAll('#contributorsTable .line');
-    for (i = 0; i < (b.length - 1); i++) {
-      shouldSwitch = false;
-      if (dir === "ASC") {
-        if (b[i].querySelectorAll("td")[1].innerHTML.toLowerCase() > b[i + 1].querySelectorAll("td")[1].innerHTML.toLowerCase()) {
-          shouldSwitch = true;
-          break;
-        }
-      } else if (dir === "DESC") {
-        if (b[i].querySelectorAll("td")[1].innerHTML.toLowerCase() < b[i + 1].querySelectorAll("td")[1].innerHTML.toLowerCase()) {
-          shouldSwitch = true;
-          break;
-        }
-      }
-    }
-    if (shouldSwitch) {
-      b[i].parentNode.insertBefore(b[i + 1], b[i]);
-      switching = true;
-      switchCount++;
-    } else {
-      if (switchCount === 0 && dir === "ASC") {
-        dir = "DESC";
-        switching = true;
-      }
-    }
-  }
+  contributors.sort(function (left, right) { return left.login.toUpperCase() == right.login.toUpperCase() ? 0 : (left.login.toUpperCase() < right.login.toUpperCase() ? -1 : 1) });
+  if (dir == "DESC")
+    contributors.reverse();
 }
-
-
-
-
 $(function () {
   start();
-  sortTable("ASC");
-
 });
